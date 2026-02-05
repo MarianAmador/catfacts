71a02d90de54d0279551dab4c0eef3474b231540
Marian Amador <142361216+MarianAmador@users.noreply.github.com>
2026-02-04 21:54:48 -0700

    feat: agregué timeout para falta de conexión

--- catfacts/js/api.js
+++ catfacts/js/api.js
@@ -1,9 +1,25 @@
 export async function getCatFact() {
-  const response = await fetch('https://catfact.ninja/fact');
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), 5000); // 5 segundos
 
-  if (!response.ok) {
-    throw new Error('Error en la respuesta de la API');
+  try {
+    const response = await fetch('https://catfact.ninja/fact', {
+      signal: controller.signal
+    });
+
+    if (!response.ok) {
+      throw new Error('Error de la API');
+    }
+
+    return await response.json();
+
+  } catch (error) {
+    if (error.name === 'AbortError') {
+      throw new Error('Tiempo de espera agotado');
+    }
+    throw error;
+
+  } finally {
+    clearTimeout(timeoutId);
   }
-
-  return response.json();
 }
